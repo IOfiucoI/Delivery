f91fe02dae16c2ea15b6290db8330368e4cf041e
unknown <marco_rg@msn.com>
2023-01-05 20:13:17 -0600

    Carpetas Corregidas

--- gulpfile.js
+++ gulpfile.js
@@ -13,42 +13,42 @@ const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
 function css( done ) {
-    src('src/scss/app.scss')
+    src('scss/app.scss')
         .pipe( sourcemaps.init() )
         .pipe( sass() )
         .pipe( postcss([ autoprefixer(), cssnano() ]) )
         .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
+        .pipe( dest('css') )
 
     done();
 }
 
 function imagenes() {
-    return src('src/img/**/*')
+    return src('archivos/**/*')
         .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+        .pipe( dest('img') )
 }
 
 function versionWebp() {
     const opciones = {
         quality: 50
     }
-    return src('src/img/**/*.{png,jpg}')
+    return src('archivos/**/*.{png,jpg}')
         .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
+        .pipe( dest('img') )
 }
 function versionAvif() {
     const opciones = {
         quality: 50
     }
-    return src('src/img/**/*.{png,jpg}')
+    return src('archivos/**/*.{png,jpg}')
         .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+        .pipe( dest('img'))
 }
 
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+    watch( 'scss/**/*.scss', css );
+    watch( 'archivos/**/*', imagenes );
 }
 
 
